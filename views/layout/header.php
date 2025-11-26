0255341f0914ad4cf7fa083fd1f0273794273006
annora1304 <naylaannora@gmail.com>
2025-11-26 18:09:31 +0700

    pesanan

--- views/layout/header.php
+++ views/layout/header.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manajemen Restoran</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="/basdat_lanjut-1/css/style.css">
+    <link rel="stylesheet" href="/basdat_lanjut-1/css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <?php include 'topbar.php'; ?>
     <?php include 'sidebar.php'; ?>
     
-    <div class="main-content">
+    <div class="main-content"> 
